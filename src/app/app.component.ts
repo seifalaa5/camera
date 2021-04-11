f0a2bb02fdd6c4fbf7ed48a4dd9edfec9c984260
seifalaa5 <Seif3la2@gmail.com>
2021-04-11 13:11:17 +0200

    adjust request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,36 +3,36 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
- 
-  @ViewChild("video")
+  @ViewChild('video')
   public video!: ElementRef;
 
-  @ViewChild("canvas")
+  @ViewChild('canvas')
   public canvas!: ElementRef;
 
   public captures: Array<any>;
 
   public constructor() {
-      this.captures = [];
+    this.captures = [];
   }
 
-  public ngOnInit() { }
+  public ngOnInit() {}
 
   public ngAfterViewInit() {
-      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-          navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-              this.video.nativeElement.src = window.URL.createObjectURL(stream);
-              this.video.nativeElement.play();
-          });
-      }
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        this.video.nativeElement.srcObject = stream;
+        this.video.nativeElement.play();
+      });
+    }
   }
 
   public capture() {
-      var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
-      this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+    var context = this.canvas.nativeElement
+      .getContext('2d')
+      .drawImage(this.video.nativeElement, 0, 0, 640, 480);
+    this.captures.push(this.canvas.nativeElement.toDataURL('image/png'));
   }
-
 }
