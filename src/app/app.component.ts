eefc95a3d853c12a4ddde2a251878665a89672ca
seifalaa5 <Seif3la2@gmail.com>
2021-04-11 12:23:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'camera';
+ 
+  @ViewChild("video")
+  public video!: ElementRef;
+
+  @ViewChild("canvas")
+  public canvas!: ElementRef;
+
+  public captures: Array<any>;
+
+  public constructor() {
+      this.captures = [];
+  }
+
+  public ngOnInit() { }
+
+  public ngAfterViewInit() {
+      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+              this.video.nativeElement.src = window.URL.createObjectURL(stream);
+              this.video.nativeElement.play();
+          });
+      }
+  }
+
+  public capture() {
+      var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+      this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+  }
+
 }
